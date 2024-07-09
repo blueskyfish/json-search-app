46bccec5ea1dee122ef1959670d71c841ba15f9a
Blueskyfish <blueskyfish@blueskyfish.de>
2024-07-09 17:12:21 +0200

    feature (css) add css root variable and default variables

--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import './Versions.css';
 
-function Versions(): JSX.Element {
-  const [versions] = useState(window.electron.process.versions)
+export default function Versions(): JSX.Element {
+  const [versions] = useState(window.electron.process.versions);
 
   return (
-    <ul className="versions">
-      <li className="electron-version">Electron v{versions.electron}</li>
-      <li className="chrome-version">Chromium v{versions.chrome}</li>
-      <li className="node-version">Node v{versions.node}</li>
+    <ul className="app-versions">
+      <li className="app-version electron-version">Electron v{versions.electron}</li>
+      <li className="app-version chrome-version">Chromium v{versions.chrome}</li>
+      <li className="app-version node-version">Node v{versions.node}</li>
     </ul>
-  )
+  );
 }
-
-export default Versions
